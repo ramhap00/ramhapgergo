93dbcd7e1929aa5eb8b95ed547895cb90435ba64
ramhap1 <ramger020@hengersor.hu>
2024-12-06 11:01:31 +0100

    calculator
    calculator

--- frontend/nodejs2/index.js
+++ frontend/nodejs2/index.js
@@ -5,5 +5,15 @@
 
 console.log(__direname);
 console.log(__filename);
+const add = require('./calculator.js'); 
+console.log(add(2,3));
 
+require('http');
+
+const server = http.createServer(function(req,res){
+    res.writeHead(200);
+    res.end('Hello World');
+});
+
+server.listen(8080);
 //}
