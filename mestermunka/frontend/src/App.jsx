fff32ca2452a6ab354a86e3ddeadca94658d5685
ramhap1 <ramger020@hengersor.hu>
2025-02-18 08:27:55 +0100

    javitasok

---
+++ mestermunka/frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      <h1>S.O.S. Munka</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
