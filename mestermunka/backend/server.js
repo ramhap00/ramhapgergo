f83d034dfb6ad3146aa58fa883430dfa8f8e183f
wagnerjanos18 <wagjan151@hengersor.hu>
2025-02-28 14:18:55 +0100

    posztok fix
    posztok fix

--- mestermunka/backend/server.js
+++ mestermunka/backend/server.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection({
   user: 'root',
   password: '',
   database: 'sos_munka',
-  port: '3307',
+  port: '3306',
 });
 
 app.use(cors({ origin: 'http://localhost:5173', credentials: true , methods: ['GET', 'POST', 'PUT', 'DELETE'], }));
